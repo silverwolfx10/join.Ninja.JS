54aaea06157de6a7039e2af35c5ed2c57833e3d4
Front-end Engineer <cleber.programmer@hotmail.com>
2015-08-21 15:25:51 -0300

    Create join.js

---
+++ join.js
@@ -0,0 +1,40 @@
+/**
+ * $join
+ * 
+ * Junta todos os elementos do array em um literal de caracteres
+ * a funcao curry
+ * 
+ * @module @join
+ * @author Cleber de Moraes Gonçalves <cleber.programmer>
+ * @example
+ * 
+ *        $join(['hello', 'cleber.programmer'], ' ');
+ * 
+ */
+Ninja.module('$join', ['$curry'], function ($curry) {
+  
+  /**
+   * Jounta todos os elementos do array em uma literal
+   * de string
+   * 
+   * @public
+   * @method join
+   * @param {Array} a Colecao de elementos
+   * @param {String} b Separador do elementos do array
+   * @return {String} Literal de caracteres
+   * @example
+   * 
+   *        $join(['hello', 'cleber.programmer'], ' ');
+   * 
+   */
+  function join(a, b) {
+    return a.join(b);
+  }
+  
+  /**
+   * Revelacao do servico $join, encapsulando a visibilidade das funcoes
+   * privadas
+   */
+  return $curry(join);
+  
+});
